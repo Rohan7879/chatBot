da8a48fc582061afb8f59e359f8d7560e15dad87
Rohan Vasoya <rohanvasoya2013@gmail.com>
2023-07-21 18:14:35 +0530

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { TranslateModule } from '@ngx-translate/core';
+
+import {
+  StreamChatModule,
+  StreamAutocompleteTextareaModule,
+} from 'stream-chat-angular';
 
 @NgModule({
   declarations: [AppComponent, DemoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TranslateModule.forRoot(),
+    StreamAutocompleteTextareaModule,
+    StreamChatModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  ChatClientService,
+  ChannelService,
+  StreamI18nService,
+} from 'stream-chat-angular';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  styleUrls: ['./demo.component.css'],
 })
 export class DemoComponent {
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService
+  ) {
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'shy-bird-4';
+    const userToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2h5LWJpcmQtNCIsImV4cCI6MTY4OTk2MDA2N30.V60kY2NtKKLOFCUyQDlcI4Re3Ct9Rcsz-qFmw85Hd18';
+    this.chatService.init(apiKey, userId, userToken);
+    this.streamI18nService.setTranslation();
+  }
 
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel(
+      'messaging',
+      'talking-about-angular',
+      {
+        // add as many custom fields as you'd like
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+        name: 'Talking about Angular',
+      }
+    );
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
 }
