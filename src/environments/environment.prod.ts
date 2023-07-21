a8ab8d37ac85974a5d7625af2fdb9e1f4303b19a
Rohan Vasoya <rohanvasoya2013@gmail.com>
2023-07-21 17:50:28 +0530

    home

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  vonageApiKey: 'YOUR_VONAGE_API_KEY',
+  vonageApiSecret: 'YOUR_VONAGE_API_SECRET',
+  vonageApplicationId: 'YOUR_VONAGE_APPLICATION_ID',
+  vonagePhoneNumber: 'YOUR_VONAGE_PHONE_NUMBER',
+  vonagePrivateKeyPath: 'PATH_TO_PRIVATE_KEY_FILE',
+};
